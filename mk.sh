d4a744ebf4c58748415dc7da50d85f4b92a74649
JavaCommons Technologies <javacommons@gmail.com>
2023-04-21 05:05:25 +0900

    .

--- mk.sh
+++ mk.sh
@@ -13,7 +13,7 @@ function processApp() {
   #exists=`$cwd/gitlab-console-x86_64-static.exe --action exists --project spider-explorer/spider-software .software/$app/$app-$version.7z`
   url=https://gitlab.com/spider-explorer/spider-software/-/raw/main/.software/$app/$app-$version.7z
   json=`echo "$json" | jq -r --arg app "$app" --arg version "$version" --arg path_spec "$path_spec" --arg url "$url" '.software[$app] = {"version":$version, "ext":"7z", "path":$path_spec, "url":$url}'`
-  if [ -e $cwd/.software/$app/$app-$version.7z ]; then
+  if [ -e $cwd/.software/$app/$app-$version.7z.001 ]; then
     return 0
   fi
   if [ -e "$path" ]; then
@@ -23,8 +23,11 @@ function processApp() {
     cd .software/$app
     cd $path
     if [ "$app" == "opera" ]; then cp -rp launcher.exe opera.exe; fi
-    7z.exe a -r $cwd/.software/$app/$app-$version.7z * -x!"User Data" -x!"profile" -x!"distribution"
+    7z.exe a -r -v3m $cwd/.software/$app/$app-$version.7z * -x!"User Data" -x!"profile" -x!"distribution"
     #$cwd/gitlab-console-x86_64-static.exe --action upload --project spider-explorer/spider-software $cwd/.software/$app/$app-$version.7z --path .software/$app/
+    if [ -e $cwd/.software/$app/$app-$version.7z ]; then
+      mv $cwd/.software/$app/$app-$version.7z $cwd/.software/$app/$app-$version.7z.001
+    fi
   fi
 }
 cwd=`pwd`
